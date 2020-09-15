401ee445ed835cc5b2b3719c97e3beaa0cf334d6
AinoGG <ryadomgg@gmail.com>
2020-09-15 23:53:38 +0300

    Первые шаги проекта

---
+++ app/js/main.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+function  tabs(tabsSelector, tabsContentSelector, tabsParentsSelector, activeClass) {
+
+    const tabs = document.querySelectorAll(tabsSelector),
+          tabsContent = document.querySelectorAll(tabsContentSelector),
+          tabsParent = document.querySelector(tabsParentsSelector);
+
+
+
+
+        function hideTabContent() {
+            tabsContent.forEach(item  =>{
+            item.classList.add('hide');
+            item.classList.remove('show');
+            });
+
+            tabs.forEach(item => {
+                item.classList.remove(activeClass);
+            })
+        }
+
+
+        function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(activeClass);
+        }
+
+
+        hideTabContent();
+        showTabContent();
+
+
+        tabsParent.addEventListener('click', event => {
+        const target = event.target;
+
+        if(target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if(target === item){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            }); 
+        }
+    });
+}
+
+
+tabs('.tabs__header-item', '.tabs__content', '.tabs__container', 'tabheader__item_active');
